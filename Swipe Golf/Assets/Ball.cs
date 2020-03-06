593e44712e9f1865441c34c2d99efa04c3ede8e1
JustBeachy <ajbeach67@gmail.com>
2020-03-05 19:18:52 -0500

    New drag system replaces the swipe & Water particles

--- Swipe Golf/Assets/Ball.cs
+++ Swipe Golf/Assets/Ball.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class Ball : MonoBehaviour
 {
     public Rigidbody2D rb;
-    float currentX, currentY, pastX, pastY;
+    float currentX, currentY, pastX, pastY,firstX,firstY;
     bool isdown = false;
     bool canSwipe = true;
     public AudioSource[] hit;
@@ -26,6 +26,7 @@ public class Ball : MonoBehaviour
     {
         controller = GameObject.FindGameObjectWithTag("GameController");
         gotHiddenCoin = false;
+        GetComponent<LineRenderer>().enabled = false;
     }
 
     // Update is called once per frame
@@ -34,6 +35,12 @@ public class Ball : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             isdown = true;
+            firstX = Input.mousePosition.x;
+            firstY = Input.mousePosition.y;
+
+            GetComponent<LineRenderer>().enabled = true;
+            GetComponent<LineRenderer>().SetPosition(0, Camera.main.ScreenToWorldPoint(new Vector3(firstX, firstY, 1)));
+            GetComponent<LineRenderer>().SetPosition(1, Camera.main.ScreenToWorldPoint(new Vector3(firstX, firstY, 1))); //reset line renderer
         }
         if(Input.GetMouseButton(0))
         {
@@ -41,6 +48,10 @@ public class Ball : MonoBehaviour
             pastY = currentY;
             currentX = Input.mousePosition.x;
             currentY = Input.mousePosition.y;
+
+            GetComponent<LineRenderer>().SetPosition(1, Camera.main.ScreenToWorldPoint(new Vector3(currentX, currentY, 1)));//stretch and color line
+            ColorLine();
+            
         }
         else
         {
@@ -48,6 +59,7 @@ public class Ball : MonoBehaviour
         }
         if (Input.GetMouseButtonUp(0))
         {
+            GetComponent<LineRenderer>().enabled = false;
             SwipeBall();
         }
     }
@@ -64,12 +76,12 @@ public class Ball : MonoBehaviour
     {
         if (Controller.Swipes > 0&&Controller.Start&&canSwipe&&!Controller.noSwipeZone)
         {
-            Vector2 velocity = new Vector2(currentX - pastX, currentY - pastY);
+            Vector2 velocity = new Vector2(currentX - firstX, currentY - firstY);
 
-            var swipeMark = Instantiate(slash, Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10), Quaternion.Euler(0, 0, Mathf.Atan2(currentY - pastY, currentX - pastX) * Mathf.Rad2Deg));
-            swipeMark.transform.localScale = new Vector3(velocity.magnitude/14, 1, 1); //make slash mark and scale it
+           // var swipeMark = Instantiate(slash, Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10), Quaternion.Euler(0, 0, Mathf.Atan2(currentY - pastY, currentX - pastX) * Mathf.Rad2Deg));
+           // swipeMark.transform.localScale = new Vector3(velocity.magnitude/14, 1, 1); //make slash mark and scale it
 
-            velocity = velocity / 1.5f;//reduce phyisical swipe sensitivity
+            velocity = velocity / (Screen.height/25);//reduce phyisical swipe sensitivity
             velocity = Vector2.ClampMagnitude(velocity, 12); //limit max speed
 
             rb.velocity = Vector3.zero;
@@ -81,6 +93,26 @@ public class Ball : MonoBehaviour
             
 
             
+        }
+    }
+
+    void ColorLine()
+    {
+        Vector2 line = new Vector2(currentX - firstX, currentY - firstY);
+        if (line.magnitude <= Screen.height / 4)
+        {
+            GetComponent<LineRenderer>().endColor = Color.green;
+            GetComponent<LineRenderer>().startColor = Color.green;
+        }
+        if (line.magnitude > Screen.height / 4&& line.magnitude < Screen.height / 2)
+        {
+            GetComponent<LineRenderer>().endColor = Color.yellow;
+            GetComponent<LineRenderer>().startColor = Color.yellow;
+        }
+        if (line.magnitude >= Screen.height / 2)
+        {
+            GetComponent<LineRenderer>().endColor = Color.red;
+            GetComponent<LineRenderer>().startColor = Color.red;
         }
     }
     
--- Swipe Golf/Assets/Water.cs
+++ Swipe Golf/Assets/Water.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
-    public float Position, Velocity;
+    public ParticleSystem particle;
 
     public void Update()
     {
        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Ball")
+        {
+            if (other.GetComponent<Rigidbody2D>().velocity.magnitude > 4)
+            {
+
+                particle.transform.position = other.transform.position;
+                particle.Play(true);
+            }
+        }
     }
 }
